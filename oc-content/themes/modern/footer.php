007ec6b5431a643ad2f7ffdb9c842ae50fc96efe
Ida Benedetto <ida@uncommonplaces.com>
2013-04-22 09:03:01 -0400

    updated footer text

--- oc-content/themes/modern/footer.php
+++ oc-content/themes/modern/footer.php
@@ -25,14 +25,14 @@
 <!-- footer -->
 <div id="footer">
     <div class="inner">
-        <a href="<?php echo osc_contact_url(); ?>"><?php _e('Contact', 'modern'); ?></a>
+        <!-- <a href="<?php echo osc_contact_url(); ?>"><?php _e('Contact', 'modern'); ?></a>
         <?php osc_reset_static_pages(); ?>
         <?php while( osc_has_static_pages() ) { ?>
             | <a href="<?php echo osc_static_page_url(); ?>"><?php echo osc_static_page_title(); ?></a>
-        <?php } ?>
+        <?php } ?> -->
         <?php
             if( osc_get_preference('footer_link', 'modern_theme') ) {
-                echo ' | ' . __('This website is proudly using the <a title="Osclass web" href="http://osclass.org/">classifieds scripts</a> software <strong>Osclass</strong>', 'modern');
+                echo  __('This website makes not claims about security of the system or your identity. Keep your wits about you!', 'modern');
             }
         ?>
     </div>
